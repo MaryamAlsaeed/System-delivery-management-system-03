2a6ec4215893e958a69fde8079aad468459b6a31
Maryam <mariamsaeedalshamy@gmail.com>
2026-09-21 00:53:25 +0400

    International shipment class

---
+++ OOP3/InternashionalShipment.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    internal class InternashionalShipment : Shipment
+    {
+        private string _destinationCountry;
+        private double _customsFee;
+        public string DestinationCountry
+        {
+            get { return _destinationCountry; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    _destinationCountry = value;
+            }
+        }
+        public double CustomsFee
+        {
+            get { return _customsFee; }
+            set
+            {
+                if (value >= 0)
+                    _customsFee = value;
+            }
+        }
+        public InternashionalShipment(string trackingCode, string description, double weight, double deliveryFee, DeliveryAddress destination)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+            DestinationCountry = _destinationCountry;
+            CustomsFee = _customsFee;
+        }
+        public override double EstimatedCost
+        {
+            get { return DeliveryFee + (Weight * 5) + CustomsFee; }
+        }
+        public virtual void GenerateCustomsReport()
+        {
+            Console.WriteLine($"\nCustoms Report for {TrackingCode}");
+            Console.WriteLine($"Destination: {DestinationCountry}");
+            Console.WriteLine($"Weight: {Weight} KG");
+            Console.WriteLine($"Customs Fee: {CustomsFee} EGP");
+            Console.WriteLine($"Estimated Cost: {EstimatedCost} EGP");
+        }
+        public override void PrintShipment()
+        {
+            Console.WriteLine("\nInternational Shipment");
+            Console.WriteLine($"Tracking Code : {TrackingCode}");
+            Console.WriteLine($"Description : {Description}");
+            Console.WriteLine($"Weight : {Weight} KG");
+            Console.WriteLine($"Delivery Fee : {DeliveryFee} EGP");
+            Console.WriteLine($"Destination Country : {DestinationCountry}");
+            Console.WriteLine($"Customs Fee : {CustomsFee} EGP");
+            Console.WriteLine($"Estimated Cost : {EstimatedCost} EGP");
+        }
+    }
+}
